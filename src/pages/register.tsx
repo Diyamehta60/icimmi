282e8f5064deec9a7f834d646932fdc1ea7c1c9c
Ayush Jangid <ayushjangid441@gmail.com>
2024-08-11 13:24:51 +0530

    rates

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -122,8 +122,8 @@ const Register = () => {
                   >
                     Ph.D Student
                   </th>
-                  <td className="px-6 py-4">INR 4,000</td>
-                  <td className="px-6 py-4">INR 5,000</td>
+                  <td className="px-6 py-4">INR 3,500</td>
+                  <td className="px-6 py-4">INR 4,500</td>
                 </tr>
                 <tr className="border-b bg-white dark:bg-gray-800">
                   <th
@@ -132,8 +132,8 @@ const Register = () => {
                   >
                     PG/UG Student
                   </th>
-                  <td className="px-6 py-4">INR 1,500</td>
-                  <td className="px-6 py-4">INR 2,000</td>
+                  <td className="px-6 py-4">INR 3,000</td>
+                  <td className="px-6 py-4">INR 3,500</td>
                 </tr>
                 <tr className="border-b bg-white dark:bg-gray-800">
                   <th
