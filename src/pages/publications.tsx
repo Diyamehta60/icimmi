ac32b8ebe864e8341d03f187d679a5ed4117cd09
codeseker <ayushjangid441@gmail.com>
2025-07-24 22:12:00 +0530

    publication remove

--- src/pages/publications.tsx
+++ src/pages/publications.tsx
@@ -18,7 +18,7 @@ function Publications() {
       </Head>
       <Schedule />
       <ImportantDetails />
-      <APCRates/>
+      {/* <APCRates/> */}
       <hr className="my-8 h-px border-0 bg-gray-200 dark:bg-gray-700" />
 
       <p>
