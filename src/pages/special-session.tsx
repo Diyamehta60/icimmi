def4aedfec02b8cf4dbde56d8014292e4bddde4c
Anil Kumar <anilvasu@gmail.com>
2025-08-06 19:24:40 +0530

    via anil sir

--- src/pages/special-session.tsx
+++ src/pages/special-session.tsx
@@ -315,17 +315,17 @@ const tracks = [
       {
         name: "Dr. Neha Bhatia",
         designation: "(Session Chair)",
-        image: "/gallery/special_sessions_old/track19/A.jpeg",
+        image: "/gallery/special_sessions_old/track19/i1.jpeg",
       },
       {
         name: "Dr. Manjula Shanbhog",
         designation: "(Session Co-Chair)",
-        image: "/gallery/special_sessions_old/track19/B.jpeg",
+        image: "/gallery/special_sessions_old/track19/i2.jpeg",
       },
       {
         name: "Dr Anuradha Konidena",
         designation: "(Session Co-Chair)",
-        image: "/gallery/special_sessions_old/track19/C.jpeg",
+        image: "/gallery/special_sessions_old/track19/i3.jpeg",
       },
      ],
   },
