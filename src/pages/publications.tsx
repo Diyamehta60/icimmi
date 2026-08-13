f19befcc976581b93764ff00c71cad4d58280f71
anil <anil.vashu@gmail.com>
2026-08-13 10:55:06 +0530

    via anil sir

--- src/pages/publications.tsx
+++ src/pages/publications.tsx
@@ -2,6 +2,7 @@ import ImportantDetails from "@/components/importantDetails";
 import Schedule from "@/components/schedule";
 import Sponsored from "@/components/sponsored";
 import Head from "next/head";
+import Image from "next/image";
 
 import React from "react";
 
@@ -18,20 +19,29 @@ function Publications() {
 
       <Schedule />
 
-      <hr className="my-8 h-px border-0 bg-gray-200 dark:bg-gray-700" />
+      <hr className="mb-0 h-px border-0 bg-gray-200 dark:bg-gray-700" />
 
-      <div className="my-8 space-y-4 leading-relaxed text-gray-800">
+      <div className="mt-0 mb-8 leading-relaxed text-gray-800">
+        <div className="flex justify-center mt-4">
+          <div className="max-w-[240px] w-full">
+            <Image
+              src="/gallery/logos/IET%20Logo.png"
+              alt="IET logo"
+              width={240}
+              height={90}
+              className="object-contain"
+            />
+          </div>
+        </div>
         <p>
-          All accepted, registered, and presented papers will be published in
-          the ICIMMI 2026 Conference Proceedings, subject to the conference&apos;s
-          publication agreement and the publisher&apos;s editorial policies.
-        </p>
-
-        <p>
-          The conference publication proposal has been submitted to leading
-          professional publishers and is currently under review. The final
-          publication partner and paper template will be announced on the
-          conference website upon approval.
+          The proceedings of ICIMMI-2026 have been approved for publication by
+          The Institution of Engineering and Technology (IET), UK. Accepted,
+          registered and presented papers will be submitted to IET for final
+          quality and integrity checks. Papers approved by IET will be
+          published in the IET Conference Proceedings, made available through
+          the IET Digital Library and IEEE Xplore, and submitted for indexing
+          in Scopus, IET Inspec and Ei Compendex. Final publication and
+          indexing are subject to IET and the respective databases' policies.
         </p>
       </div>
 
